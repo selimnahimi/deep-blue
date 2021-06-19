378a6ad8a802ff893aa8ec41000d78833a51922d
HUNcamper <selimnahimi@gmail.com>
2021-06-19 19:35:27 +0200

    valgrind analyze

--- analyze.sh
+++ analyze.sh
@@ -1,9 +1,8 @@
 clear
-export LD_LIBRARY_PATH=.:$PWD/lib
 valgrind --leak-check=full \
          --show-leak-kinds=all \
          --track-origins=yes \
          --verbose \
          --log-file=valgrind-out.txt \
-         ./a.out
+         ./build/main.a
 cat valgrind-out.txt
